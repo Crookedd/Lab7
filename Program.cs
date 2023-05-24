b413cf6f1d278f5084b21bbead24911244e21290
LAPTOP-ULCU732A\User <valeri.ignateva@list.ru>
2023-05-24 23:09:18 +0300

    lol

--- Program.cs
+++ Program.cs
@@ -1,31 +1,15 @@
-﻿using Lab7;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace Lab7
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        BinaryTree<int> integerTree = new BinaryTree<int>();
-
-        Random rand = new Random();
-
-        for (int Index = 0; Index < 20; ++Index)
+        static void Main(string[] args)
         {
-            int value = rand.Next(100);
-            Console.WriteLine("Adding {0}", value);
-            integerTree.Add(value);
         }
-        Console.WriteLine("Preorder traversal:");
-        Console.WriteLine(string.Join(" ", integerTree.Preorder()));
-        Console.WriteLine("Postorder traversal:");
-        Console.WriteLine(string.Join(" ", integerTree.Postorder()));
-        Console.WriteLine("Levelorder traversal:");
-        Console.WriteLine(string.Join(" ", integerTree.Levelorder()));
-        Console.WriteLine("Inorder traversal:");
-        foreach (int value in integerTree)
-            Console.Write("{0} ", value);
-        Console.WriteLine();
-        Console.ReadKey(true);
     }
 }
